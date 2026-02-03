5bd424ac471375183f42dcb41cbe62e997d2412a
Aiden West <aiden.d.west@gmail.com>
2026-02-03 16:17:02 -0500

    current build

--- client/app/(tabs)/about.tsx
+++ client/app/(tabs)/about.tsx
@@ -1,9 +1,12 @@
 import { Text, View, StyleSheet } from 'react-native';
-
+import { Link } from 'expo-router';
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>About Screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to Home Screen
+      </Link>
     </View>
   );
 }
@@ -18,5 +21,10 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#eee',
+  },
+  button: {
+    fontSize: 25,
+    textDecorationLine: 'underLine',
+    color: '#000',
   },
 });
